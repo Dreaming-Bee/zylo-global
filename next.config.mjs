cee65eb56b1a59883ee9e2cce65847a7debb1a72
Dreaming-Bee <bsgunaweera16@gmail.com>
2025-05-16 14:03:31 +0530

    Update next.config.mjs with basePath and assetPrefix; add homepage to package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/zylo-global',
+  assetPrefix: '/zylo-global',
   eslint: {
     ignoreDuringBuilds: true,
   },
